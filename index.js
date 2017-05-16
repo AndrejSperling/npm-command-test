1b6f567fc6806a00147eb5c5e5afb07dcba9ed20
Andrej <sperling@protonmail.com>
2017-05-16 21:21:58 +0200

    jo

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+const mkdirp = require('mkdirp');
+const path = require('path');
 
-var mkdirp = require('mkdirp');
 
-function makeConsoleOutput(){
-	console.log("Hey!!!")
+function makeConsoleLog() {
+    console.log("Hey!!!")
+    console.log("Du befindest dich gerade hier: " + process.cwd())
 }
 
-function buildWebApp(){
-	mkdirp('/public/folder', function (err) {
-		if (err) console.error(err)
-		else console.log('pow!')
-	});
+function buildWebApp() {
+    const folder = path.resolve(process.cwd(), './public');
+    mkdirp(folder, function (err) {
+        if (err) console.error(err)
+        else console.log('Created Folder: ' + folder)
+    });
 }
 
 module.exports = {
-    makeConsoleLog: makeConsoleOutput,
+    makeConsoleLog: makeConsoleLog,
     buildWebApp: buildWebApp
 };
